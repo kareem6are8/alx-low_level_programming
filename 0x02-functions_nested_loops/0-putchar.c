d710bd8ad5f06670cef059653543179c132d742d
kareem <kareemtarek29@gmail.com>
2023-03-21 19:13:07 -0700

    lol

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,25 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	char *sh = "Holberton";
+char ch;
+int i;
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+i = 0;
 
-	return (0);
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
 }
-
+_putchar('\n');
+i++;
+}
+}
