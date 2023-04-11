426a870b998bbc86764f8c9c97baa1e21006cd24
kareem <kareemtarek29@gmail.com>
2023-04-10 23:53:08 -0700

    r

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * str_checker - Compare two strings
- * @s1: fisrt string
- * @s2: second string
- * Return: returns 1 if the strings can be considered identical, otherwise 0
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-int str_checker(char *s1, char *s2)
+
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
-			return (str_checker(s1, s2 + 1));
+			return (wildcmp(s1, s2 + 1));
 		}
 		return (*s2 == '\0');
 	}
 
 	if (*s2 == '*')
 	{
-		return (str_checker(s1 + 1, s2) || str_checker(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
-		return (str_checker(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
