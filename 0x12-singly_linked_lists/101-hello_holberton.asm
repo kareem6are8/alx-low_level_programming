6413c45acb1160f073e1b0e1f3efa6c1437d7757
kareem <kareemtarek29@gmail.com>
2023-05-03 11:27:03 -0700

    some changes

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
-section .text
-	global main
 
-main:
-	mov	edx, len
-	mov	ecx, msg	;message variable
-	mov 	ebx, 1		;write to standard output
-	mov	eax, 4
-	int	0x80		;call kernel
-
-	mov	eax, 1 		;system call (sys_exit)
-	int	0x80		;call kernel
-
-section .data
-msg db 'Hello, Coding', 0xa	;message to print
-len equ $ - msg			;length of the string
-
+section .text;
+default rel;
+extern printf;
+global main;
+main;
+push rbp;
+mov rdi, format;
+mov rsi, string;
+mov rax, 0;
+call printf wrt ..plt;
+pop rbp;
+mov rax, 0;
+ret;
+section .data;
+string: db "Hello, Holberton", 10, 0
+format: db "%s", 0
